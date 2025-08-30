e2ca25a678ebce36fcc9faa5379f70e9bfba560f
sarragm <gmatisarra2004@gmail.com>
2025-08-30 23:06:16 +0100

    Add files via upload

--- front/miniJira/src/app/ticket-list/ticket-list.component.ts
+++ front/miniJira/src/app/ticket-list/ticket-list.component.ts
@@ -1,37 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute,Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Ticket } from '../models/ticket.model';
 import { CommonModule } from '@angular/common';
 import { TicketService } from '../ticket.service';
-import { HeaderComponent } from '../header/header.component';
 
 
 @Component({
   selector: 'app-ticket-list',
   standalone:true,
-  imports: [CommonModule,HeaderComponent],
+  imports: [CommonModule],
   templateUrl: './ticket-list.component.html',
   styleUrl: './ticket-list.component.css'
 })
-export class TicketListComponent implements OnInit{
-  constructor(private ticS:TicketService,private route: ActivatedRoute,private router: Router){}
+export class TicketListComponent {
   Tickets!: Ticket[];
-  st : string|null='';
-  ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      const status = params.get('status');
-      this.st=status;
-
-      if (status) {
-        this.ticS.getTiks(status).subscribe(res => this.Tickets = res);
-      }
-      else{
-        alert('No tickets with that status !');
-      }
-    });
+  ticket!: Ticket;
+  router!: Router;
+  ticS!:TicketService;
+  
+  importerTickets(status:string){
+    this
+    
   }
 
   allerVersTicket(idT : string){
-    this.router.navigate(['/ticket/id',idT])
+    this.router.navigate(['/ticket',idT])
   }
 }
